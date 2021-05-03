47e443903924371ecd05775d24bbace81c3c9fa5
CandraJengger <cakjengger@gmail.com>
2021-05-04 05:55:07 +0700

    Create Login Page with FormLogin

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { Row, Col } from 'antd';
+import { Form, Input, Button, Card } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 const Login = () => {
-  return <div>Ini Login</div>;
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = (values) => {
+    setLoading(true);
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Row justify="center" align="middle" style={{ height: '100vh' }}>
+        <Col xs={24} md={12} lg={8}>
+          <Card>
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  { required: true, message: 'Please input your Username!' },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Username"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: 'Please input your Password!' },
+                ]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                  loading={loading}
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 Login.layout = 'plain';
