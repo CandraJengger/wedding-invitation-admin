d52b10a54a70b6ff41824835b285f586af4d8589
CandraJengger <cakjengger@gmail.com>
2021-05-04 13:54:09 +0700

    Roleback commit

--- pages/api/public/[id].js
+++ pages/api/public/[id].js
@@ -1,96 +1,97 @@
 import prisma from '../../../lib/prisma';
 
 export default async (req, res) => {
-  const namePerson = req.query.name;
-
-  if (req.method === 'GET') {
-    try {
-      const wish = await prisma.invitation.findMany({
-        where: {
-          AND: [
-            {
-              name: {
-                equals: namePerson,
-              },
-            },
-            {
-              attending: {
-                equals: true,
-              },
-            },
-          ],
-        },
-      });
-
-      if (wish.length == 0) {
-        return res.status(404).json({
-          success: false,
-          error: {
-            message: `Not Found!`,
-          },
-        });
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: {
-          wish,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        success: false,
-        error: {
-          message: `Internal server error!`,
-        },
-      });
+    const invitId = req.query.id;
+    
+    if (req.method === 'GET') {
+        try {
+            const wish = await prisma.invitation.findMany({
+                where : {
+                    AND : [
+                        {
+                            id_invitation : {
+                                equals : Number(invitId)
+                            }
+                        },
+                        {
+                            attending : {
+                                equals : true
+                            }
+                        }
+                    ]
+                }
+            });
+    
+            if (wish.length == 0) {
+                return res.status(404).json({
+                    success : false,
+                    error : {
+                        message : `Not Found!`
+                    }
+                });
+            }
+    
+            return res.status(200).json({
+                success : true,
+                data : {
+                    wish
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                success : false,
+                error : {
+                    message : `Internal server error!`
+                }
+            });
+        }
+    } else if (req.method === 'PUT') {
+        const {wish, attending} = req.body;
+    
+        try {
+            const updateInvit = await prisma.invitation.update({
+                where : {
+                    id_invitation : Number(invitId)
+                }, 
+                data : {
+                    wish : wish,
+                    attending : attending     
+                }
+            });
+    
+            if (!updateInvit) {
+                return res.status(404).json({
+                    success : false,
+                    error : {
+                        message : `Not Found!`
+                    }
+                });
+            }
+    
+            return res.status(200).json({
+                success : true,
+                data : {
+                    message : `Success update data`
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            if (error.code == 'P2025') {
+                return res.status(404).json({
+                    success : false,
+                    error : {
+                        message : error.meta.cause
+                    }
+                });
+            }
+            return res.status(500).json({
+                success : false,
+                error : {
+                    message : `Internal server error!`
+                }
+            })
+        }
     }
-  } else if (req.method === 'PUT') {
-    const { wish, attending } = req.body;
-
-    try {
-      const updateInvit = await prisma.invitation.update({
-        where: {
-          name: namePerson,
-        },
-        data: {
-          wish: wish,
-          attending: attending,
-        },
-      });
-
-      if (!updateInvit) {
-        return res.status(404).json({
-          success: false,
-          error: {
-            message: `Not Found!`,
-          },
-        });
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: {
-          message: `Success update data`,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      if (error.code == 'P2025') {
-        return res.status(404).json({
-          success: false,
-          error: {
-            message: error.meta.cause,
-          },
-        });
-      }
-      return res.status(500).json({
-        success: false,
-        error: {
-          message: `Internal server error!`,
-        },
-      });
-    }
-  }
-};
+    
+}
--- pages/api/public/index.js
+++ pages/api/public/index.js
@@ -1,46 +1,47 @@
 import prisma from '../../../lib/prisma';
 
 export default async (req, res) => {
-  if (req.method === 'GET') {
-    const name = req.query.name;
+    if (req.method === 'GET') {
+        try {
+            const wishes = await prisma.invitation.findMany({
+                where : {
+                    show : true
+                },
+                orderBy : {
+                    id_invitation : "desc"
+                }
+            });
 
-    try {
-      const wishes = await prisma.invitation.findUnique({
-        where: {
-          name: name,
-        },
-      });
+            if (!wishes) {
+                return res.status(404).json({
+                    success : false,
+                    error : {
+                        message : `Not Found!`
+                    }
+                });
+            }
 
-      if (!wishes) {
-        return res.status(404).json({
-          success: false,
-          error: {
-            message: `Not Found!`,
-          },
+            return res.status(200).json({
+                success : true,
+                data : {
+                    wishes
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                success : false,
+                error : {
+                    message : `Internal server error`
+                }
+            })
+        }
+    } else {
+        return res.status(405).json({
+            success : false,
+            error : {
+                message : `Method ${req.method} not allow!`
+            }
         });
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: {
-          wishes,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        success: false,
-        error: {
-          message: `Internal server error`,
-        },
-      });
     }
-  } else {
-    return res.status(405).json({
-      success: false,
-      error: {
-        message: `Method ${req.method} not allow!`,
-      },
-    });
-  }
-};
+}
