8929505ad603a3b37cb3702c27680b440430951a
CandraJengger <cakjengger@gmail.com>
2021-05-08 05:15:07 +0700

    Implement Logout

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Layout } from 'antd';
+import Router from 'next/router';
+import axios from 'axios';
+import { Layout, Button } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
 
 const HeaderComponent = () => {
-  return <Header className="site-layout-background" style={{ padding: 0 }} />;
+  const handleLogout = () => {
+    axios
+      .post(
+        'http://localhost:3000/api/logout',
+        {},
+        {
+          headers: {
+            cookie: 'token',
+          },
+        }
+      )
+      .then(() => {
+        Router.push('/login');
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  return (
+    <Header
+      className="site-layout-background"
+      style={{ padding: 0, textAlign: 'right' }}
+    >
+      <Button onClick={handleLogout} style={{ marginRight: '16px' }}>
+        <LogoutOutlined />
+        Logout
+      </Button>
+    </Header>
+  );
 };
 
 export default HeaderComponent;
