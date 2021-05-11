8927352ebca351755d306a7b13a626ace9b8586a
Pram4dafi <aldindzikri@gmail.com>
2021-05-11 18:11:35 +0700

    fix setHeader cookie

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,12 +2,12 @@
 import cookie from 'cookie';
 
 export default (req, res) => {
-  res.setHeader('Set-Cookie', cookie.serialize('foo', 'haweuh34', {
-    httpOnly: true,
-    secure : process.env.NODE_ENV !== "development",
-    maxAge : 60 * 60,
-    sameSite: "strict",
-    path: "/"
-  }));
-  res.status(200).json({ name: 'John Doe' })
+  // res.setHeader('Set-Cookie', cookie.serialize('foo', 'haweuh34', {
+  //   httpOnly: true,
+  //   secure : process.env.NODE_ENV !== "development",
+  //   maxAge : 60 * 60,
+  //   sameSite: "strict",
+  //   path: "/"
+  // }));
+  // res.status(200).json({ name: 'John Doe' })
 }
--- pages/api/logout.js
+++ pages/api/logout.js
@@ -14,7 +14,6 @@ export default (req, res) => {
     }
 
     res.setHeader('Set-Cookie', cookie.serialize('tokenAccess', '', {
-        httpOnly: true,
         secure : process.env.NODE_ENV !== "development",
         expires: new Date(0),
         sameSite: "strict",
